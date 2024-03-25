c6fa3f510b236fb0cea9dda5c48cc66a049fd0cc
gcsimao <gustavocsimao@gmail.com>
2024-03-25 13:36:59 -0300

    Quartz sync: Mar 25, 2024, 1:36 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,7 +16,7 @@ export default ((opts?: Options) => {
         <hr />
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://intranet.mprj.mp.br/administrativo/informatica/">STIC MPRJ
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
