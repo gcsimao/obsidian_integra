092a1643765f77e0722bf2b83b47f27c5bbe8d6f
gcsimao <gustavocsimao@gmail.com>
2024-03-25 17:58:56 -0300

    Quartz sync: Mar 25, 2024, 5:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -45,7 +45,7 @@ const config: QuartzConfig = {
           dark: "#ebebec",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          highlight: "f6f1eb",
         },
       },
     },
